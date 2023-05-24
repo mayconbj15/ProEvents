31f696d1a2a9fe25c6c84d8b19aa25eedec2ca64
mayconbj15 <mayconbj15@gmail.com>
2023-05-23 23:11:59 -0300

    Get events from API

--- src/front/ProEvents-App/src/app/app.module.ts
+++ src/front/ProEvents-App/src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { HttpClientModule } from '@angular/common/http'
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { EventsComponent } from './events/events.component';
@@ -14,7 +14,8 @@ import { SpeakersComponent } from './speakers/speakers.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/front/ProEvents-App/src/app/events/events.component.ts
+++ src/front/ProEvents-App/src/app/events/events.component.ts
@@ -1,3 +1,4 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
 
 @Component({
@@ -6,5 +7,16 @@ import { Component } from '@angular/core';
   styleUrls: ['./events.component.scss']
 })
 export class EventsComponent {
+  public events: any;
 
+  constructor(private httpClient: HttpClient){
+    this.getEvents();
+  }
+
+  public getEvents(): any {
+    this.httpClient.get('http://localhost:5289/api/events').subscribe(
+      response => this.events = response,
+      error => console.error(error)
+    );
+  }
 }
